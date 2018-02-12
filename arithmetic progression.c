1377c195d2dd42b6cb6ee2349e7b881581b9f46c
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-12 16:47:11 +0530

    Create arithmetic progression.c

---
+++ arithmetic progression.c
@@ -0,0 +1,14 @@
+#include,stdio.h>
+int main()
+{
+int n,a,d,i,sum=1;
+printf("enter the values:\n",n,a,d);
+scanf("%d %d %d",&n,&a,&d);
+for(i=n;i>1;i--)
+{
+sum=sum+((n-1)*d);
+}
+sum=sum+a;
+printf("arithmetic progression is %d",sum0;
+return 0;
+}
