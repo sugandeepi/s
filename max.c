16fda38e2f973eb09ae5248bb9e0bf953a3b6b19
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-12 17:00:55 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+	int m,a,b,c;
+	scanf("%d",&m);
+	scanf("%d %d %d",&a,&b,&c);
+	if(a>b&&a>c)
+	{
+		printf("%d",a);
+	}
+	else if(b>a&&b>c)
+	   {
+		printf("%d",b);
+	   }
+     else
+	{
+		printf("%d",c);
+	}
+	return 0;
+}
