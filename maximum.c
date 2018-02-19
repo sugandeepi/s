278f23ab6a2b46a28c6fdb095663573bde8d0924
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-19 16:29:28 +0530

    Create maximum.c

---
+++ maximum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int array[20],size,i,largest;
+{
+printf("enter the size of an array");
+scanf("%d",&size);
+printf("enter the elements %d in an array");
+for(i=0;i<size;i++)
+scanf("%d",&array[i]);
+largest=array[0];
+for("i=1;i<size;i++)
+{
+if(largest<array[i]);
+largest=array[i]);
+}
+printf("the largest element in an array is %d",largest);
+return o;
+}
+
+
