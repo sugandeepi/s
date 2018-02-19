05c92ef62f3836fb32129697c3cc00dbf89dd4a6
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-19 16:04:52 +0530

    Create count.c

---
+++ count.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char b[30];
+int i;
+printf("enter the string");
+scanf("%s\n",&b[i]);
+for(i=0;b[i]!='\0';++i)
+{
+printf("total character in the string is %d",i);
+}
+return 0;
+}
