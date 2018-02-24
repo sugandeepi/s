ba3363d3e8ca3c702b6290e8f48e2c809462422f
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-24 07:51:54 -0800

    Create new file

---
+++ words.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,word=1;
+printf("enter the string\n");
+gets(str);
+for(i=0;i<strlen(str);i++)
+{
+if(str[i]==' ');
+{
+word++;
+}
+}
+printf("%d\n",word);
+return 0;
+}
