69a1577ca2f91359dd8407a8c5a17030c4eaa013
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-25 06:53:43 -0800

    Create numeric char.c

---
+++ numeric char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int i,digit=0;
+char str[100];
+printf("enter the string:");
+gets(str)
+for(i=0;i<strlen(str);i++)
+{
+if(str[i]>'0'&&str[i]<'9')
+{
+digit++;
+}
+}
+printf("%d",digit)
+}
