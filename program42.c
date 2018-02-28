34d49c95398a7212de30d3f0e57276a1210914dd
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-28 16:40:51 +0530

    Create program42.c

---
+++ program42.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    char a[50],b[50];
+    printf("enter the two strings:");
+    scanf("%s %s",a,b);
+    if(a>b)
+    {
+        printf("%s",a);
+        
+    }
+else
+{
+    printf("%s",b);
+}
+}
