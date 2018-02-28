b7fa56ea9b9d9afae23ddb7df8944571966f70a4
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-28 16:11:12 +0530

    Create program44.c

---
+++ program44.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    printf("enter the number:");
+    scanf("%d",&n);
+        if((n>=1)&&(n<=10))
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+        }
+    
