db2abf0ed9e99427c78621c1b920caf9f4d79f91
sugandeepi <31506087+sugandeepi@users.noreply.github.com>
2018-02-28 09:50:40 -0800

    Create program46.c

---
+++ program46.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    {
+        i=n+1;
+    }
+printf("the next number is %d",i);
+return 0;
+}
